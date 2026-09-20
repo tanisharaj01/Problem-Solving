49c6850edbe386d2fa07556183e3557ee1d3e8be
TANISHA RAJ <158483134+tanisharaj01@users.noreply.github.com>
2026-09-20 12:57:25 +0530

    Time: 617 ms (97.83%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select Department,Employee,Salary
+from (select d.name as Department,e.name as Employee,e.salary as Salary, 
+dense_rank() over (partition by d.name order by e.salary desc) as rnk
+from Employee e
+inner join Department d
+on e.departmentId =d.id)t
+where rnk=1;
+
